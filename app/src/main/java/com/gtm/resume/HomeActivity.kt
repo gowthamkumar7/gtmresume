865f90373c54337102a13cde45155929662f9a2c
Gowtham <gtm516@gmail.com>
2018-03-11 19:14:57 +0530

    Header completed

--- app/src/main/java/com/gtm/resume/HomeActivity.kt
+++ app/src/main/java/com/gtm/resume/HomeActivity.kt
@@ -1,15 +1,57 @@
 package com.gtm.resume
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import android.view.View
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.content_about.*
+import kotlinx.android.synthetic.main.content_header.*
 
+
+/**
+ *
+ * @author Gowtham kumar
+ *
+ * Home screen for this application
+ *
+ */
 class HomeActivity : AppCompatActivity() {
 
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-    textview.text = "Hello world"
-  }}
+
+        //Load display picture
+        display_picture.loadImageCenterCrop();
+
+        email.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_EMAIL, "gtm516@gmail.com");
+
+            intent.setData(Uri.parse("mailto:"))
+            startActivity(Intent.createChooser(intent, "Send an email to gowtham using"))
+        }
+        test.setOnClickListener {
+
+            layout.visibility = View.GONE;
+        }
+    }
+
+
+    fun ImageView.loadImageCenterCrop() {
+        val circle = CircleTransformation();
+        Picasso.with(this.getContext())
+                .load(R.drawable.display_pic_gtm)
+                .centerCrop()
+                .fit()
+                .transform(circle)
+                .into(this)
+    }
+}
